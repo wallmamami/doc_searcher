b11f865cfdecaf1055ad551f348b43f6b66c3b40
wallmamami <911968336@qq.com>
2018-08-29 13:22:46 +0800

    add

--- common/util.hpp
+++ common/util.hpp
@@ -22,6 +22,79 @@ public:
         boost::split(*output, input, boost::is_any_of(split_char), boost::token_compress_off);
     }
 };
+
+class DicUtil
+{
+public:
+    //从cppjieba暂停词文件中加载暂停词的内容
+    //一行为一个暂停词，这里将一个暂停词作为set的一个元素
+    bool Load(const std::string& path)
+    {
+        std::ifstream file(path.c_str());
+        if(!file.is_open())
+        {
+            //这里打印日志不适用LOG(FATAL)，主要是因为这个类
+            //可能会在很多不同场景下使用到，所以错误级别交给调用者来决定
+            return false;
+        }
+
+        std::string line;
+        while(std::getline(file, line))
+        {
+            set_.insert(line);
+        }
+
+        return true;
+    }
+
+    bool Find(const std::string& key)const
+    {
+        return set_.find(key) != set_.end();
+    }
+
+private:
+    std::unordered_set<std::string> set_;
+};
+
+class FileUtil
+{
+public:
+    //把 content 中的所有内容都写到文件当中
+    static bool Write(const std::string& output_path, const std::string& content)
+    {
+        std::ofstream file(output_path.c_str());
+        if(!file.is_open())
+        {
+            return false;
+        }
+
+        file.write(content.data(), content.size());
+        file.close();
+        return true;
+    }
+
+    //把文件的所有内容都读到content中
+    static bool Read(const std::string& input_path, std::string* content)
+    {
+        std::ifstream file(input_path.c_str());
+        if(!file.is_open())
+        {
+            return false;
+        }
+
+        //需要先获取到文件的长度
+        //把文件光标放到文件末尾
+        file.seekg(0, file.end);
+        //获取到文件光标的位置
+        int len = file.tellg();
+        file.seekg(0, file.beg);
+        content->resize(len);
+        file.read(const_cast<char*>(content->data()), len);
+        file.close();
+        return true;
+    }
+};
+
 } //end common
 
 
